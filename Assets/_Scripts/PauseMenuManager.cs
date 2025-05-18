c8fe2c633fe08204ed65507c106a2dc8afda721d
Rica003 <rica.COortega03@gmail.com>
2025-05-18 17:04:08 -0300

    Script das Funcionalidades do menu de pause dentro da Scene do JogoTeste

---
+++ Assets/_Scripts/PauseMenuManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenu;
+    private bool isPaused = false;
+
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+}
