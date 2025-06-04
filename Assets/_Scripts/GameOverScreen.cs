e015b9cf3826823206280377bd250fd648d19753
Ricardo de Oliveira Ortega <rica.COortega03@gmail.com>
2025-06-03 22:35:32 -0300

    script que gerencia a tela de Game Over

--- Assets/_Scripts/GameOverScreen.cs
+++ Assets/_Scripts/GameOverScreen.cs
@@ -1,14 +1,40 @@
-using UnityEngine;
-using System.Collections;
-using UnityEngine.UI;
+﻿using UnityEngine.UI;
 using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOverScreen : MonoBehaviour
 {
     public TMP_Text pointsText;
+    public TMP_Text construcoesText; // ✅ Novo campo para mostrar construções
+    [SerializeField] private string nomeDoLevelDeJogo;
+    [SerializeField] private string VoltarMenu;
+    [SerializeField] private GameObject painelBackground;
 
-    public void Setup(int score){
+    private void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void Setup(float score, int construcoes) // ✅ Recebe construções também
+    {
         gameObject.SetActive(true);
-        pointsText.text = score.ToString() + " POINTS";
+        pointsText.text = score.ToString("F2") + " Recursos";
+
+        if (construcoesText != null)
+        {
+            construcoesText.text = construcoes + " Construções";
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(nomeDoLevelDeJogo);
+        Debug.Log("O Jogo foi Reiniciado");
+    }
+
+    public void MenuPrincipal()
+    {
+        SceneManager.LoadScene(VoltarMenu);
     }
 }
